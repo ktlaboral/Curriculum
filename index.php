7eead16563316a0f6a9f2390b098a9e3800bdb01
ktlaboral <69206305+ktlaboral@users.noreply.github.com>
2020-10-20 16:51:16 -0500

    Casi completado

--- index.php
+++ index.php
@@ -21,13 +21,13 @@
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
 
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+      <span class="navbar-toggler-icon"> </span>
     </button>
 
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-      <ul class="navbar-nav mx-auto">
+      <ul class="navbar-nav ml-sm-auto mx-auto">
 
         <li class="nav-item <?php if ($pagina == "Inicio") {
                               echo "active";
@@ -44,13 +44,13 @@
         <li class="nav-item">
           <a class="nav-link  <?php if ($pagina == "Proyectos") {
                                 echo "active";
-                              } ?> " href="proyectos.php"> Proyectos </a>
+                              } ?> " href="#proyectos"> Proyectos </a>
         </li>
 
         <li class="nav-item">
           <a class="nav-link  <?php if ($pagina == "Sobre mi") {
                                 echo "active";
-                              } ?> " href="sobre_mi.php">Sobre mi</a>
+                              } ?> " href="#sobre_mi">Sobre mi</a>
         </li>
 
 
@@ -88,13 +88,13 @@
 
     <!-- Boton contratame -->
     <div class="padre_boton position-relative">
-      <a href="#" class="text-center boton position-relative w-100 h-100">Contrátame</a>
+      <a href="#" class="text-center boton position-relative w-100 h-100" data-toggle="modal" data-target="#contratame">Contrátame</a>
     </div>
     <!--  -->
 
     <!-- Boton ver CV -->
     <div class="ml-3 padre_boton position-relative">
-      <a href="#" class="text-center boton position-relative w-100 h-100">Ver CV</a>
+      <a target="_blank" href="#" class="text-center boton position-relative w-100 h-100">Ver CV</a>
     </div>
     <!--  -->
 
@@ -102,21 +102,25 @@
   <!--  -->
 
   <!-- Acerca de mi -->
-  <section class="container mt-5 d-flex align-items-center justify-content-center flex-wrap">
+  <section id="sobre_mi" class="container mt-5 d-flex overflow-hidden align-items-center justify-content-center flex-wrap">
 
-    <div class="col-12 col-lg-6 col-xl-6">
+    <div class="imagen_presentacion col-12 col-lg-6 col-xl-6">
 
       <img class="w-100" src="img/sobre_mi.png" alt="Sobre mi">
 
     </div>
 
-    <div class="col-12 col-lg-6 col-xl-6">
+    <div class="texto_presentacion col-12 col-lg-6 col-xl-6">
 
-      <h2 class="text-center"> Déjame presentarme </h2>
+      <h2 class="cursive text-center font-weight-bold"> Déjame presentarme </h2>
 
-      <p class="mt-5 text-justify"> Mi nombre es Andrés Mauricio, actualmente tengo 21 años, soy desarrollador web desde hace casi 2 años y desde hace medio año empecé como free lancer (Desarrollador independiente). </p>
 
-      <p class="text-justify "> Algo que siempre me ha caracterizado es el ser autodidacta, gracias a ello he podido aprender mucho en lo que me apasiona, la informatica. Mi objetivo es brindar un servicio aplicando siempre buenas practicas, empeño y por sobre todo entusiasmo por lo que hago, a cada trabajo le empleo un 110% para realizar un trabajo excelente.  </p>
+
+
+
+      <p class="mt-5 text-justify"> Mi nombre es Andrés, actualmente tengo 21 años, soy desarrollador Web desde hace 2 años y desde hace medio año empecé como free lancer (Desarrollador independiente). </p>
+
+      <p class="text-justify "> Algo que siempre me ha caracterizado es el ser autodidacta, gracias a ello he podido aprender mucho en lo que me apasiona, la informática. Mi objetivo es brindar un servicio aplicando siempre buenas practicas, empeño y por sobre todo entusiasmo por lo que hago, a cada trabajo le empleo un 110% para realizar un trabajo excelente. </p>
 
     </div>
 
@@ -124,8 +128,7 @@
   <!--  -->
 
   <!-- Habilidades -->
-
-  <h1 class="text-center mt-5"> Mis habilidades </h1>
+  <h2 class="cursive text-center font-weight-bold mt-5"> Mis habilidades </h2>
 
   <section class="mt-5 container d-flex flex-wrap">
 
@@ -214,6 +217,116 @@
     </div>
     <!--  -->
 
+    <!-- BOOTSTRAP -->
+    <div class="col-12 col-lg-6 col-xl-6 mb-5">
+
+      <div class="d-flex w-100 align-items-baseline justify-content-center">
+
+        <i class="fab fa-bootstrap mr-2"></i>
+        <h6 class="text-center"> Bootstrap (80%) </h6>
+
+      </div>
+
+      <div class="progress">
+        <div id="bootstrap" class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+      </div>
+
+    </div>
+    <!--  -->
+
+
+  </section>
+  <!--  -->
+
+
+  <!-- Servicios ofrecidos -->
+  <section class="container mt-5">
+
+    <h2 class="cursive text-center font-weight-bold"> Servicios que ofrezco </h2>
+
+    <div class="my-5 d-flex flex-wrap align-items-start justify-content-center">
+
+      <div class="caja_iconos col-12 col-lg-3 col-xl-3 d-flex flex-column align-items-center justify-content-lg-start justify-content-xl-start justify-content-center">
+        <img class="my-2 i_servicios" src="img/sitio_web.png" alt="">
+        <h6 class="font-italic text-center"> Diseño y desarrollo de páginas Web </h6>
+        <p class="text-center"> Diseño y desarrollo de páginas Web profesionales que se adaptan a las necesidades de tu negocio. Paginas 100% responsive, intuitivas y rápidas, así como la que estas viendo. </p>
+      </div>
+
+      <div class="caja_iconos col-12 col-lg-3 col-xl-3 d-flex flex-column align-items-center justify-content-lg-start justify-content-xl-start justify-content-center">
+        <img class="my-2 i_servicios" src="img/supervision.png" alt="">
+        <h6 class="font-italic text-center"> Mantenimiento de equipos de computo </h6>
+        <p class="text-center"> Diagnostico y mantenimiento de equipos computacionales. Actualización de sistemas operativos, análisis en busca de fallas o defectos, entre otros... </p>
+      </div>
+
+      <div class="caja_iconos col-12 col-lg-3 col-xl-3 d-flex flex-column align-items-center justify-content-lg-start justify-content-xl-start justify-content-center">
+        <img class="my-2 i_servicios" src="img/pc.png" alt="">
+        <h6 class="font-italic text-center"> Montar PC para trabajo u oficina </h6>
+        <p class="text-center"> Tanto si buscas un computador para edición, trabajo o simplemente para jugar, puedo ayudarte a buscar los componentes perfectos para ti y entregarte tu computador listo para trabajar. </p>
+      </div>
+
+    </div>
+
+  </section>
+  <!--  -->
+
+
+  <!-- Proyectos -->
+  <section id="proyectos" class="container">
+
+    <h2 class="my-5 cursive text-center font-weight-bold"> Algunos de mis proyectos realizados </h2>
+
+    <!-- Grupo cards -->
+    <div class="d-flex flex-wrap align-items-center justify-content-center">
+
+      <!-- Card -->
+      <a target="_blank" href="http://www.luxurysdk.com/" class="proyecto p-0 col-11 col-lg-3 col-xl-3 mt-3 mt-lg-0 mt-xl-0 mx-2">
+
+        <div class="cabecera">
+          <img loading="lazy" src="img/luxurys.jpg" alt="">
+        </div>
+
+        <div class="cuerpo my-2">
+          <h5 class="text-center"> Luxury's DK </h5>
+          <p class="text-center font-italic"> Variedades </p>
+        </div>
+
+      </a>
+      <!--  -->
+
+      <!-- Card -->
+      <a target="_blank" href="http://www.rionegociosdelsur.com/" class="proyecto p-0 col-11 col-lg-3 col-xl-3 mt-3 mt-lg-0 mt-xl-0 mx-2">
+
+        <div class="cabecera">
+          <img loading="lazy" src="img/rio_negocios.jpg" alt="">
+        </div>
+
+        <div class="cuerpo my-2">
+          <h5 class="text-center"> Rio negocios del sur </h5>
+          <p class="text-center font-italic"> Página de inmobiliaria </p>
+        </div>
+
+      </a>
+      <!--  -->
+
+      <!-- Card -->
+      <a target="_blank" href="http://www.inmobiliariasantafedc.com/index.php" class="proyecto p-0 col-11 col-lg-3 col-xl-3 mt-3 mt-lg-0 mt-xl-0 mx-2">
+
+        <div class="cabecera">
+          <img loading="lazy" src="img/santafe.jpg" alt="">
+        </div>
+
+        <div class="cuerpo my-2">
+          <h5 class="text-center"> Gomez y asociados </h5>
+          <p class="text-center font-italic"> Página de inmobiliaria </p>
+        </div>
+
+      </a>
+      <!--  -->
+
+
+    </div>
+    <!--  -->
+
 
   </section>
   <!--  -->
@@ -221,7 +334,47 @@
 
 
 
+  <footer class="mt-5 d-flex align-items-center justify-content-center">
+
+    <p class="text-center"> Diseñado y Desarrollado por <a href="tel:+57 3114451314">Andrés Duque</a> ©Copyright 2020. Todos los derechos reservados. </p>
+
+  </footer>
+
+
+
+
+  <!-- Boton para subir -->
+  <div id="btn_subir">
+    <i class="fas fa-arrow-up"></i>
+  </div>
+  <!--  -->
+
+
+  <!-- Modal -->
+  <div class="modal fade" id="contratame" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          ...
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--  -->
+
+
+
   <?php include_once "layout/archivos_footer.php" ?>
-  <script src="js/prueba.js"></script>
+  <script src="js/script.js"></script>
 
 </body>
