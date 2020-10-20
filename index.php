ba572c1ea18778ba3c03a9df78d16b78344e19e2
ktlaboral <69206305+ktlaboral@users.noreply.github.com>
2020-10-19 23:29:24 -0500

    Se avanzó hasta mis habilidades

--- index.php
+++ index.php
@@ -62,7 +62,7 @@
   <!--  -->
 
   <!-- Vista Principal -->
-  <section id="vista_principal" class="d-flex">
+  <section id="vista_principal" class="d-flex flex-wrap">
 
     <div class="col-6 p-0 m-0 d-none d-lg-block d-xl-block">
       <img class="imagenes_fondo" src="img/desarrollador2.jpg" alt="">
@@ -76,6 +76,50 @@
       <img class="imagen_perfil w-100 h-auto" src="img/user.jpg" alt="">
     </div>
 
+    <h6 class="desarrollador"> Desarrollador full stack </h6>
+
+  </section>
+  <!--  -->
+
+
+  <!-- Botones de contratame -->
+  <div class="d-flex mt-4 flex-wrap align-items-center justify-content-center">
+
+
+    <!-- Boton contratame -->
+    <div class="padre_boton position-relative">
+      <a href="#" class="text-center boton position-relative w-100 h-100">Contrátame</a>
+    </div>
+    <!--  -->
+
+    <!-- Boton ver CV -->
+    <div class="ml-3 padre_boton position-relative">
+      <a href="#" class="text-center boton position-relative w-100 h-100">Ver CV</a>
+    </div>
+    <!--  -->
+
+  </div>
+  <!--  -->
+
+  <!-- Acerca de mi -->
+  <section class="container mt-5 d-flex align-items-center justify-content-center flex-wrap">
+
+    <div class="col-12 col-lg-6 col-xl-6">
+
+      <img class="w-100" src="img/sobre_mi.png" alt="Sobre mi">
+
+    </div>
+
+    <div class="col-12 col-lg-6 col-xl-6">
+
+      <h2 class="text-center"> Déjame presentarme </h2>
+
+      <p class="mt-5 text-justify"> Mi nombre es Andrés Mauricio, actualmente tengo 21 años, soy desarrollador web desde hace casi 2 años y desde hace medio año empecé como free lancer (Desarrollador independiente). </p>
+
+      <p class="text-justify "> Algo que siempre me ha caracterizado es el ser autodidacta, gracias a ello he podido aprender mucho en lo que me apasiona, la informatica. Mi objetivo es brindar un servicio aplicando siempre buenas practicas, empeño y por sobre todo entusiasmo por lo que hago, a cada trabajo le empleo un 110% para realizar un trabajo excelente.  </p>
+
+    </div>
+
   </section>
   <!--  -->
 
@@ -84,7 +128,6 @@
   <h1 class="text-center mt-5"> Mis habilidades </h1>
 
   <section class="mt-5 container d-flex flex-wrap">
-
 
     <!-- HTML 5 -->
     <div class="col-12 col-lg-6 col-xl-6 mb-5">
@@ -172,11 +215,10 @@
     <!--  -->
 
 
-
   </section>
   <!--  -->
 
-  <div style="height:2000px;"></div>
+
 
 
   <?php include_once "layout/archivos_footer.php" ?>
